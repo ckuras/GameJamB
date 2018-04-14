141017040aa4eaf0b7c8bfd13055df3789bf647d
unknown <ckuras@stevens.edu>
2018-04-14 16:00:04 -0400

    First commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    //Groups
+    public GameObject[] groups;
+
+    public void spawnNext()
+    {
+        //random index
+        int i = Random.Range(0, groups.Length);
+
+        //spawn group at current position
+        Instantiate(groups[i], transform.position, Quaternion.identity);
+    }
+
+	void Start () {
+        //spawn initial group
+        spawnNext();
+	}
+	
+
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/blockType.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockType : MonoBehaviour {
+
+    public enum blocktype {attack, heal, magic};
+
+    public blocktype thisType;
+
+    public blocktype getType()
+    {
+        return thisType;
+    }
+
+
+}
